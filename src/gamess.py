1a0c8c124e0b2edda3b36bbbd870957347e99c20
Casper Steinmann <steinmann@chem.ku.dk>
2012-05-25 15:41:51 +0200

    Update license to allow for later version.

--- src/gamess.py
+++ src/gamess.py
@@ -7,14 +7,20 @@ Some portions Copyright (C) 2011-2012 Casper Steinmann
 
 This file is part of the FragIt project.
 
-This program is free software; you can redistribute it and/or modify
+FragIt is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation version 2 of the License.
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
+FragIt is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+02110-1301, USA.
 ***********************************************************************/
 """
 from numpy import sqrt, dot, where, array
