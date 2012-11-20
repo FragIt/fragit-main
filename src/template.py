e51fbbe4919ee8bfbcac888cedc9cf6124492357
Casper Steinmann <casper.steinmann@gmail.com>
2012-11-20 11:00:09 +0100

    Correct name for pymol script. Fixes #5

--- src/template.py
+++ src/template.py
@@ -29,97 +29,102 @@ import os
 
 import util
 
+# filenames for templates
+filenames = {'pymol':'pml',
+             'jmol' :'jmol'}
+
 class Template(object):
-	def __init__(self, infile, outfile):
-		self.infile = infile
-		self.outfile = outfile
-		self.data = list() # holds the template
-		self.fragments_data = list()
-		self.buffer_data = list()
-		self.active_data = list()
-		self.backbone_data = list()
-		self.template_type = None
-		self.load_structure_string = None
+    def __init__(self, infile, outfile):
+        self.infile = infile
+        self.outfile = outfile
+        self.data = list() # holds the template
+        self.fragments_data = list()
+        self.buffer_data = list()
+        self.active_data = list()
+        self.backbone_data = list()
+        self.template_type = None
+        self.load_structure_string = None
 
-	def _setTemplateType(self,value):
-		if not util.is_string(value): raise ValueError("Template type is a string value.")
-		self.template_type = value
+    def _setTemplateType(self,value):
+        if not util.is_string(value): raise ValueError("Template type is a string value.")
+        if not filenames.has_key(value): raise ValueError("Template type '%s' is not valid." % value)
+        self.template_type = value
 
-	def _setLoadStructureString(self,value):
-		if not util.is_string(value): raise ValueError("Load structure string is a string value.")
-		self.load_structure_string = value + "\n"
+    def _setLoadStructureString(self,value):
+        if not util.is_string(value): raise ValueError("Load structure string is a string value.")
+        self.load_structure_string = value + "\n"
 
-	def setFragmentsData(self, value):
-		self.fragments_data = value
+    def setFragmentsData(self, value):
+        self.fragments_data = value
 
-	def setBufferData(self, value):
-		self.buffer_data = value
+    def setBufferData(self, value):
+        self.buffer_data = value
 
-	def setActiveData(self, value):
-		self.active_data = value
+    def setActiveData(self, value):
+        self.active_data = value
 
-	def setBackboneData(self, value):
-		self.backbone_data = value
+    def setBackboneData(self, value):
+        self.backbone_data = value
 
-	def setPairData(self,value):
-		self.fragpairs = value
+    def setPairData(self,value):
+        self.fragpairs = value
 
-	def formatSingleFragment(self, fragment_data):
-		raise NotImplementedError
+    def formatSingleFragment(self, fragment_data):
+        raise NotImplementedError
 
-	def formatFragments(self, fragments):
-		raise NotImplementedError
+    def formatFragments(self, fragments):
+        raise NotImplementedError
 
-	def formatBackbone(self):
-		raise NotImplementedError
+    def formatBackbone(self):
+        raise NotImplementedError
 
-	def formatBuffer(self):
-		raise NotImplementedError
+    def formatBuffer(self):
+        raise NotImplementedError
 
-	def formatActive(self):
-		raise NotImplementedError
+    def formatActive(self):
+        raise NotImplementedError
 
-	def formatBreakPoints(self):
-		raise NotImplementedError
+    def formatBreakPoints(self):
+        raise NotImplementedError
 
-	def override(self):
-		self._readTemplateFile()
-		if self.load_structure_string is None: raise ValueError("Load structure string not defined")
-		for i,line in enumerate(self.data):
-			if "<<fragments>>" in line:
-				self.data.insert(i,self.formatFragments())
-				self.data.remove(line)
-			if "<<buffer>>" in line:
-				self.data.insert(i,self.formatBuffer())
-				self.data.remove(line)
-			if "<<active>>" in line:
-				self.data.insert(i,self.formatActive())
-				self.data.remove(line)
-			if "<<backbone>>" in line:
-				self.data.insert(i,self.formatBackbone())
-				self.data.remove(line)
+    def override(self):
+        self._readTemplateFile()
+        if self.load_structure_string is None: raise ValueError("Load structure string not defined")
+        for i,line in enumerate(self.data):
+            if "<<fragments>>" in line:
+                self.data.insert(i,self.formatFragments())
+                self.data.remove(line)
+            if "<<buffer>>" in line:
+                self.data.insert(i,self.formatBuffer())
+                self.data.remove(line)
+            if "<<active>>" in line:
+                self.data.insert(i,self.formatActive())
+                self.data.remove(line)
+            if "<<backbone>>" in line:
+                self.data.insert(i,self.formatBackbone())
+                self.data.remove(line)
 
-			if "<<breakpoints>>" in line:
-				self.data.insert(i,self.formatBreakPoints())
-				self.data.remove(line)
+            if "<<breakpoints>>" in line:
+                self.data.insert(i,self.formatBreakPoints())
+                self.data.remove(line)
 
-			if "<<pdbfilename>>" in line:
-				self.data.insert(i,self.load_structure_string % (self.infile))
-				self.data.remove(line)
+            if "<<pdbfilename>>" in line:
+                self.data.insert(i,self.load_structure_string % (self.infile))
+                self.data.remove(line)
 
-	def _readTemplateFile(self):
-		filename = self._getTemplateFilename()
-		template = open(filename,'r')
-		self.data = template.readlines()
+    def _readTemplateFile(self):
+        filename = self._getTemplateFilename()
+        template = open(filename,'r')
+        self.data = template.readlines()
 
-	def _getTemplateFilename(self):
-		if self.template_type is None: raise ValueError("Template type not set.")
-		template_file = "%s_template" % (self.template_type)
-		template_path = os.path.dirname(util.__file__)
-		return os.path.join(template_path,template_file)
+    def _getTemplateFilename(self):
+        if self.template_type is None: raise ValueError("Template type not set.")
+        template_file = "%s_template" % (self.template_type)
+        template_path = os.path.dirname(util.__file__)
+        return os.path.join(template_path,template_file)
 
-	def write(self):
-		if self.template_type is None: raise ValueError("Template type not set.")
-		f = open("%s.%s" % (self.outfile, self.template_type), 'w')
-		f.writelines(self.data)
-		f.close()
+    def write(self):
+        if self.template_type is None: raise ValueError("Template type not set.")
+        f = open("%s.%s" % (self.outfile, filenames[self.template_type]), 'w')
+        f.writelines(self.data)
+        f.close()
